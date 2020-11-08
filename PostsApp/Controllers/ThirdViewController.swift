819afb1651dc9d36eef3b5de39c0950b30a87e88
SemaDevpy <syimykdev@gmail.com>
2020-11-08 15:38:24 +0600

    add new vc

---
+++ PostsApp/Controllers/ThirdViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ThirdViewController.swift
+//  PostsApp
+//
+//  Created by Syimyk on 11/8/20.
+//  Copyright © 2020 Syimyk. All rights reserved.
+//
+
+import UIKit
+
+class ThirdViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        print("Created this vc Just for testing purposes git branching")
+    }
+    
+
+    
+
+}
